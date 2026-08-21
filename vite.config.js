682004957a9a91f0b2347ff7ca6067f831a956fc
saigraphicdesings-png <saigraphicdesings@gmail.com>
2026-08-21 16:31:53 +0530

    Remove Cloudflare plugin from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [cloudflare()],
-
   build: {
     rollupOptions: {
       input: {
