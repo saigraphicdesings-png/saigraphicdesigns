61059254e4180cf981a651334ca0e68e39e80a4b
Sai Graphic Designs <saigraphicdesings@gmail.com>
2026-09-08 18:21:30 +0530

    Use favicon for logo service cart thumbnails

--- shop.js
+++ shop.js
@@ -1736,7 +1736,7 @@ document.addEventListener("DOMContentLoaded", function () {
                                     src="${escapeHTML(
                                         item.image ||
                                         (/logo/i.test(item.name || "")
-                                            ? "Images/logo-before.png"
+                                            ? "Images/favicon.png"
                                             : /brand/i.test(item.name || "")
                                                 ? "Images/Designing.png"
                                                 : /social/i.test(item.name || "")
