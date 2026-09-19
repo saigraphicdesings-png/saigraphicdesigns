7bc43b4ea43be372d7bd68f962eb7d324ef1c184
Sai Graphic Designs <saigraphicdesings@gmail.com>
2026-09-19 13:48:32 +0530

    Refresh quick ball icon fix

--- global-animations.js
+++ global-animations.js
@@ -178,7 +178,7 @@
 
   document.addEventListener('click', shopFreeAccessCapture, true);
   document.addEventListener('click', logoutCustomer);
-  loadStyle("sai-10-10.css?v=20260919-refined-quickball");
+  loadStyle("sai-10-10.css?v=20260919-real-menu-icon");
   loadScript("global-animations-core.js?v=20260915-2");
   loadScript("customer-payment-notifications.js?v=20260919-1");
 
